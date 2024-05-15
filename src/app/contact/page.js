043ca326e3e4b12b15cd3a38e6823d79ec815950
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-05-15 13:03:48 +0600

    Basic Routing Concept And Nested Routing

---
+++ src/app/contact/page.js
@@ -0,0 +1,9 @@
+const ContactUsPage = () => {
+  return (
+    <div>
+      <h1>This is contact page/route</h1>
+    </div>
+  );
+};
+
+export default ContactUsPage;
---
+++ src/app/news/blog/page.js
@@ -0,0 +1,9 @@
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>This is Blog Page/route</h1>
+    </div>
+  );
+};
+
+export default BlogPage;
