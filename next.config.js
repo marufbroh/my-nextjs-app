0e12f10de261616ad6f5eb6571e63b6a6981043b
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-07-07 16:13:06 +0600

    Optimize Image And Fonts In Next

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["nextjs.org"]
-    }
+  images: {
+    // domains: ["nextjs.org"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        // port: "",
+        // pathname: "/my-bucket/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,12 +1,21 @@
 import Image from "next/image";
 import React from "react";
+import nextImage from "../assets/image.webp"
 
 const GalleryPage = () => {
   return (
     <div>
-      <h1 className="text-4xl text-center">Image Optimizations</h1>
+      <h1 className="text-4xl text-center">Image Optimizations external</h1>
       <Image
         src="https://nextjs.org/_next/image?url=%2Flearn%2Fdark%2Fdashboard.png&w=1920&q=75"
+        alt="next image"
+        width={500}
+        height={500}
+        className="mx-auto"
+      />
+      <h1 className="text-4xl text-center">Image Optimizations internal</h1>
+      <Image
+        src={nextImage}
         alt="next image"
         width={500}
         height={500}
