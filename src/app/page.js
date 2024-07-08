7e04b5fd99dfd9447a655441293cb76518375eb7
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-07-08 12:31:06 +0600

    Explain SSG By Next-Level Data Fetching

--- src/app/page.js
+++ src/app/page.js
@@ -6,22 +6,31 @@ export const metadata = {
 };
 
 const HomePage = async () => {
-  const res = await fetch("http://localhost:5000/posts");
-  const posts = await res.json();
+  const res = await fetch("http://localhost:5000/shoes", {
+    cache: "force-cache",
+  });
+  const shoes = await res.json();
 
   // console.log(data);
 
   return (
     <div>
       <h1>Welcome to Next.js Home Page</h1>
-      <div className="flex justify-around p-5">
-        {posts.map((post) => (
-          <div key={post.id} className="card bg-base-100 w-80 shadow-xl">
+      <div className="grid grid-cols-4 justify-items-center gap-10 p-10">
+        {shoes.map((shoe) => (
+          <div key={shoe.id} className="card bg-base-100 w-96 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
             <div className="card-body">
-              <h2 className="card-title">ID: {post.id}</h2>
-              <p>Title: {post.title}</p>
+              <h2 className="card-title">{shoe.title}</h2>
+              <p>{shoe.description}</p>
               <div className="card-actions justify-end">
-                <button className="btn btn-primary">Views: {post.views}</button>
+                <button className="btn btn-primary">Buy Now</button>
+                <button className="btn btn-primary">Details</button>
               </div>
             </div>
           </div>
